de698ae5bb0867efe063aad0ad814db5a1b805c0
adixyaxo <aditya0dagar@gmail.com>
2025-11-17 20:54:52 +0530

    adding user and computer moves

--- variable.c
+++ variable.c
@@ -3,23 +3,25 @@
 #include <time.h>
 
 int umove() {
-    int p;
+    int u;
     printf("Play your moves\nEnter 1 for rock\nEnter 2 for paper\nEnter 3 for scissor\nEnter 0 to exit game\n\t:");
-    scanf("%d", &p);
-    int t = time(NULL);
-    int c = ((rand() * t) % 3) + 1;
-    return 0;
+    scanf("%d", &u);
+    return u;
 }
 
 int cmove() {
     srand(time(NULL));
     int rno= rand();
-    printf(rand());
     int c = ((rand()) % 3) + 1;
     return c;
 }
 
 int main()
 {
-    printf("%d,cmove()");
+    printf("%d",cmove());
+    printf("%d",cmove());
+    printf("%d",cmove());
+    printf("%d",cmove());
+    printf("%d",cmove());
+    return 0;
 }
