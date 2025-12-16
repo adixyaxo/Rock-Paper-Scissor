43825eee428716ad9834525e2134a4e6ea6f401e
adixyxo <aditya0dagar@gmail.com>
2025-12-16 22:18:45 +0530

    added ui in variable.c

--- variable.c
+++ variable.c
@@ -4,13 +4,14 @@
 #include <windows.h>
 #include <unistd.h>
 #include "stats.c"
+#include "ui.h"
 // imported game to use it in the loop jisse ham pata kar paen ki loop ko kitni bar chalana hai
 
 char umove()
 {
     char u;
-    printf("\n\n---PLAY YOUR MOVES---\nEnter 1 or R for rock\nEnter 2 or P for paper\nEnter 3 or S for scissor\nEnter M to return to Main Menu\nEnter Q to quit current game\nEnter T to display current stats\nEnter ESC or 27 to exit application\nPlay your move :: ");
-    scanf(" %c", &u);
+    printui("\n\n---PLAY YOUR MOVES---\nEnter 1 or R for rock\nEnter 2 or P for paper\nEnter 3 or S for scissor\nEnter M to return to Main Menu\nEnter Q to quit current game\nEnter T to display current stats\nEnter ESC or 27 to exit application\nPlay your move :: ");
+    scanui(" %c", &u);
     sleep(1);
     return u;
 }
@@ -59,7 +60,7 @@ int match()
         // checking for esc
         int usmove = user_move;
 
-        printf("Computer chose: %d\n", comp);
+        printui("Computer chose: %d\n", comp);
 
         if (user_move != '1' && user_move != '2' && user_move != '3' &&
             user_move != 'R' && user_move != 'P' && user_move != 'S' &&
@@ -68,23 +69,23 @@ int match()
             user_move != 'm' && user_move != 'q' && user_move != 't' &&
             user_move != 27)
         {
-            printf("Invalid input \n Please try again\n");
+            printui("Invalid input \n Please try again\n");
             sleep(1);
             continue;
         }
         else if (usmove == 27) // ESC
         {
-            printf("Exiting the game...\n");
+            printui("Exiting the game...\n");
             exit(0);
         }
         else if (user_move == 'Q' || user_move == 'q')
         {
-            printf("Skipping to next round...\n");
+            printui("Skipping to next round...\n");
             continue;
         }
         else if (user_move == 'M' || user_move == 'm')
         {
-            printf("Returning to Main Menu...\n");
+            printui("Returning to Main Menu...\n");
             break;
         }
         else if (user_move == 'T' || user_move == 't')
@@ -99,7 +100,7 @@ int match()
                  ((user_move == 'P' || user_move == 'p') && comp == 2) ||
                  ((user_move == 'S' || user_move == 's') && comp == 3))
         {
-            printf("\nRESULT :: This is a tie\n");
+            printui("\nRESULT :: This is a tie\n");
             tie();
             sleep(1);
         }
@@ -111,13 +112,13 @@ int match()
             ((user_move == 'P' || user_move == 'p') && comp == 1) ||
             ((user_move == 'S' || user_move == 's') && comp == 2))
         {
-            printf("\nRESULT :: Congratulations You Win\n");
+            printui("\nRESULT :: Congratulations You Win\n");
             win();
             sleep(1);
         }
         else
         {
-            printf("\nRESULT :: You Lost\n");
+            printui("\nRESULT :: You Lost\n");
             lost();
             sleep(1);
         }
@@ -155,15 +156,15 @@ int multiplayer()
         char p1_move;
         char p2_move;
 
-        printf(" %s, enter your move: ", player1.name);
+        printui(" %s, enter your move: ", player1.name);
         p1_move = umove();
         system("cls"); // Clear krdega screen ko taki player 2 ko pata na chale ki player 1 ne kya choose kiya hai
-        printf(" %s, enter your move: ", player2.name);
+        printui(" %s, enter your move: ", player2.name);
         p2_move = umove();
         system("cls");
 
-        printf("%s chose: %c\n", player1.name, p1_move);
-        printf("%s chose: %c\n", player2.name, p2_move);
+        printui("%s chose: %c\n", player1.name, p1_move);
+        printui("%s chose: %c\n", player2.name, p2_move);
         // checking for esc
         int us1move = p1_move;
         int us2move = p2_move;
@@ -181,25 +182,25 @@ int multiplayer()
              p2_move != 'm' && p2_move != 'q' && p2_move != 't' &&
              p2_move != 27))
         {
-            printf("Invalid input \n Please try again\n");
+            printui("Invalid input \n Please try again\n");
             sleep(1);
             continue;
         }
         else if (us1move == 27 || us2move == 27) // ESC
         {
-            printf("Exiting the game...\n");
+            printui("Exiting the game...\n");
             exit(0);
         }
         else if (p1_move == 'Q' || p1_move == 'q' ||
                  p2_move == 'Q' || p2_move == 'q')
         {
-            printf("Skipping to next round...\n");
+            printui("Skipping to next round...\n");
             continue;
         }
         else if (p1_move == 'M' || p1_move == 'm' ||
                  p2_move == 'M' || p2_move == 'm')
         {
-            printf("Returning to Main Menu...\n");
+            printui("Returning to Main Menu...\n");
             break;
         }
         else if (p1_move == 'T' || p1_move == 't' ||
@@ -215,7 +216,7 @@ int multiplayer()
                  ((p1_move == 'P' || p1_move == 'p') && p2_move == '2') ||
                  ((p1_move == 'S' || p1_move == 's') && p2_move == '3'))
         {
-            printf("\nRESULT :: This is a tie\n");
+            printui("\nRESULT :: This is a tie\n");
             mtie(difficulty_level);
             sleep(1);
             roundsplayed();
@@ -228,14 +229,14 @@ int multiplayer()
             ((p1_move == 'P' || p1_move == 'p') && p2_move == '1') ||
             ((p1_move == 'S' || p1_move == 's') && p2_move == '2'))
         {
-            printf("\nRESULT :: Player 1 Wins\n");
+            printui("\nRESULT :: Player 1 Wins\n");
             mp1win(difficulty_level);
             sleep(1);
             roundsplayed();
         }
         else
         {
-            printf("\nRESULT :: Player 2 Wins\n");
+            printui("\nRESULT :: Player 2 Wins\n");
             mp2win(difficulty_level);
             sleep(1);
             roundsplayed();
